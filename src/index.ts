7d4c0c05d2f0dabf0d71537b593070add16e0497
Jeyi Adole <meetjeyvers@gmail.com>
2026-03-28 17:57:57 +0100

    Stellar addresses for SEP-02 Federation Server

--- src/index.ts
+++ src/index.ts
@@ -61,6 +61,7 @@ import { HealthCheckResponse, ReadinessCheckResponse } from "./types/api";
 import sep31Router from "./stellar/sep31";
 import sep24Router from "./stellar/sep24";
 import { createSep12Router } from "./stellar/sep12";
+import { createFederationRouter, buildStellarToml } from "./stellar/sep02";
 
 // 1. Import Sentry Middleware
 import { initSentry, sentryBreadcrumbMiddleware } from "./middleware/sentry";
@@ -252,6 +253,12 @@ app.use("/api/admin", requireAuth, adminRoutes);
 app.use("/sep31", sep31Router);
 app.use("/sep24", sep24Router);
 app.use("/sep12", createSep12Router(pool));
+app.use("/federation", createFederationRouter(pool));
+app.get("/.well-known/stellar.toml", (_req, res) => {
+  res.setHeader("Content-Type", "text/plain");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.send(buildStellarToml());
+});
 
 app.use(
   (
